c065066ac04df710992209e8b7c66102e67ffccc
Geoff Selby <geoff.w.selby@gmail.com>
2019-02-03 21:29:18 -0600

    Make it work everywhere (I hope)

--- src/core.js
+++ src/core.js
@@ -1,30 +1,35 @@
-
+const fs = require('fs');
 const cpdir = require('copy-dir');
 const { spawn } = require('child_process');
 
 module.exports = {
-    build(directory) {
-        let templateDir = `${__dirname}/templates/default`;
-        let targetDir;
+  build(directory) {
+    let targetDir;
 
-        if (typeof directory !== 'undefined') {
-            targetDir = process.cwd() + '/' + directory;
-        } else {
-            targetDir = process.cwd();
-        }
-
-        cpdir(templateDir, targetDir, function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.info('Installing dependencies...');
-                console.info('This may take some time...');
-
-                spawn(`cd ${targetDir} && yarn && yarn run dev`, [], {
-                    shell: true,
-                    stdio: 'inherit'
-                });
-            }
-        });
+    if (typeof directory !== 'undefined') {
+      targetDir = `${process.cwd()}/${directory}`;
+    } else {
+      targetDir = process.cwd();
     }
+
+    this.buildStatic(targetDir);
+  },
+
+  buildStatic(targetDir) {
+    const templateDir = `${__dirname}/templates/default`;
+
+    cpdir(templateDir, targetDir, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.info('Installing dependencies...');
+        console.info('This may take some time...');
+
+        spawn(`cd ${targetDir} && yarn && yarn run dev`, [], {
+          shell: true,
+          stdio: 'inherit'
+        });
+      }
+    });
+  },
 };
